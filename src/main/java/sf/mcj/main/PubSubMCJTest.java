bce9b030b46f4e68acf9adfa0b6e4de60bd595e0
pushpdeep <pushpdeep@google.com>
2025-12-02 17:23:58 +0530

    new subscriber tests

--- src/main/java/sf/mcj/main/PubSubMCJTest.java
+++ src/main/java/sf/mcj/main/PubSubMCJTest.java
@@ -23,19 +23,24 @@ public class PubSubMCJTest {
   private static final Logger logger = LoggerFactory.getLogger(PubSubMCJTest.class);
 
   public static void main(String[] args) throws IOException {
-     PS_PUB_AUTH_01();
-     PS_PUB_AUTH_02();
-     PS_PUB_REG_01();
-     PS_PUB_REG_02();
-     PS_PUB_MSG_01();
-     PS_PUB_MSG_03();
-     PS_PUB_MSG_04();
-     PS_PUB_MSG_05();
-     PS_PUB_MSG_06();
-     PS_PUB_MSG_07();
-     PS_PUB_MSG_08();
-     PS_PUB_MSG_09();
-     PS_PUB_MSG_10();
+    PS_PUB_AUTH_01();
+    PS_PUB_AUTH_02();
+    PS_PUB_REG_01();
+    PS_PUB_REG_02();
+    PS_PUB_MSG_01();
+    PS_PUB_MSG_03();
+    PS_PUB_MSG_04();
+    PS_PUB_MSG_05();
+    PS_PUB_MSG_06();
+    PS_PUB_MSG_07();
+    PS_PUB_MSG_08();
+    PS_PUB_MSG_09();
+    PS_PUB_MSG_10();
+    PS_SUB_AUTH_01();
+    PS_SUB_AUTH_02();
+    PS_SUB_REG_01();
+    PS_SUB_MSG_01();
+    PS_SUB_MSG_02();
   }
 
   private static void PS_PUB_AUTH_01() {
@@ -126,12 +131,12 @@ public class PubSubMCJTest {
       tempTopicClient.send(message);
       tempTopicClient.close();
 
-      logger.info(String.format("[%s] Message published at regional endpoint. Test PASSED!", scenario));
+      logger.info(String.format("[%s] Message published at regional endpoint. Test FAILED!", scenario));
     }
     catch (Exception e) {
       logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
       e.printStackTrace();
-      logger.error(String.format("[%s] Test FAILED.", scenario));
+      logger.error(String.format("[%s] Test PASSED.", scenario));
     }
   }
 
@@ -249,13 +254,13 @@ public class PubSubMCJTest {
           .build();
 
       List<Message> messages = List.of(Message.builder()
-          .withBody("Hello, sending message 1".getBytes())
-          .withMetadata(Map.of("key1", "value1", "key2", "value2"))
-          .build(),
+              .withBody("Hello, sending message 1".getBytes())
+              .withMetadata(Map.of("key1", "value1", "key2", "value2"))
+              .build(),
           Message.builder()
-          .withBody("Hello, sending message 2".getBytes())
-          .withMetadata(Map.of("key1", "value1", "key2", "value2"))
-          .build(),
+              .withBody("Hello, sending message 2".getBytes())
+              .withMetadata(Map.of("key1", "value1", "key2", "value2"))
+              .build(),
           Message.builder()
               .withBody("Hello, sending message 3".getBytes())
               .withMetadata(Map.of("key1", "value1", "key2", "value2"))
@@ -394,14 +399,235 @@ public class PubSubMCJTest {
   }
 
   private static void PS_SUB_AUTH_01() {
+    // Run below command to enable ADC before running this test.
+    // gcloud auth application-default login
     String scenario = "PS_SUB_AUTH_01";
-    URI endpoint = URI.create("https://pubsub.googleapis.com/v1");
     try {
-      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
-          .withSubscriptionName("projects/sdv-demo-env/subscriptions/mcj-topic-sub")
+      //send message
+      TopicClient tempTopicClient = TopicClient.builder("gcp")
+          .withTopicName(String.format("projects/%s/topics/%s", PROJECT_ID, TOPIC))
           .build();
+
+      String textMessage = String.format("Test Message for %s", scenario);
+
+      Message messageToBeSent = Message.builder()
+          .withBody(textMessage.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      tempTopicClient.send(messageToBeSent);
+      tempTopicClient.close();
+
+      //receive message
+      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
+          .withSubscriptionName(String.format("projects/%s/subscriptions/%s", PROJECT_ID, SUBSCRIPTION))
+          .build();
+      Message message = subscriptionClient.receive();
+      String data = new String(message.getBody());
+      System.out.println("Received: " + data);
+      subscriptionClient.sendAck(message.getAckID());
       subscriptionClient.close();
 
+      logger.info(String.format("[%s] Test PASSED!!", scenario));
+    }
+    catch (Exception e) {
+      logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
+      e.printStackTrace();
+      logger.error(String.format("[%s] Test FAILED.", scenario));
+    }
+  }
+
+  private static void PS_SUB_AUTH_02() {
+    // Run below command to disable ADC before running this test.
+    // gcloud auth application-default revoke
+    String scenario = "PS_SUB_AUTH_02";
+    try {
+      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
+          .withSubscriptionName(String.format("projects/%s/subscriptions/%s", PROJECT_ID, SUBSCRIPTION))
+          .build();
+      //subscriptionClient.close();
+      Message message = subscriptionClient.receive();
+      String data = new String(message.getBody());
+      System.out.println("Received: " + data);
+      subscriptionClient.sendAck(message.getAckID());
+      subscriptionClient.close();
+
+      logger.info(String.format("[%s] Test FAILED!!", scenario));
+    }
+    catch (Exception e) {
+      logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
+      e.printStackTrace();
+      logger.error(String.format("[%s] Exception Verified. Test PASSED.", scenario));
+    }
+  }
+
+  private static void PS_SUB_REG_01() {
+    //enable message Enforce in transit on topic before running this test.
+    String scenario = "PS_SUB_REG_01";
+    URI regionalEndpoint = URI.create("pubsub.us-central1.rep.googleapis.com:443");
+    try {
+      //send message
+      TopicClient tempTopicClient = TopicClient.builder("gcp")
+          .withTopicName(String.format("projects/%s/topics/%s", PROJECT_ID, TOPIC))
+          .withEndpoint(regionalEndpoint)
+          .build();
+
+      String textMessage = String.format("Test Message for %s", scenario);
+
+      Message messageToBeSent = Message.builder()
+          .withBody(textMessage.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      tempTopicClient.send(messageToBeSent);
+      tempTopicClient.close();
+
+      //receive message
+      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
+          .withSubscriptionName(String.format("projects/%s/subscriptions/%s", PROJECT_ID, SUBSCRIPTION))
+          .withEndpoint(regionalEndpoint)
+          .build();
+      Message receivedMessage = subscriptionClient.receive();
+      String data = new String(receivedMessage.getBody());
+      System.out.println("Received: " + data);
+      subscriptionClient.sendAck(receivedMessage.getAckID());
+      subscriptionClient.close();
+
+      logger.info(String.format("[%s] Test PASSED!!", scenario));
+    }
+    catch (Exception e) {
+      logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
+      e.printStackTrace();
+      logger.error(String.format("[%s] Test FAILED.", scenario));
+    }
+  }
+
+  private static void PS_SUB_MSG_01() {
+    String scenario = "PS_SUB_MSG_01";
+    URI regionalEndpoint = URI.create("pubsub.us-central1.rep.googleapis.com:443");
+    try {
+      //send message
+      TopicClient tempTopicClient = TopicClient.builder("gcp")
+          .withTopicName(String.format("projects/%s/topics/%s", PROJECT_ID, TOPIC))
+          .withEndpoint(regionalEndpoint)
+          .build();
+
+      String textMessageSent = String.format("Test Message for %s", scenario);
+
+      Message messageToBeSent = Message.builder()
+          .withBody(textMessageSent.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      tempTopicClient.send(messageToBeSent);
+      tempTopicClient.close();
+
+      //receive message
+      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
+          .withSubscriptionName(String.format("projects/%s/subscriptions/%s", PROJECT_ID, SUBSCRIPTION))
+          .withEndpoint(regionalEndpoint)
+          .build();
+      Message receivedMessage = subscriptionClient.receive();
+      String receivedTextMessage = new String(receivedMessage.getBody());
+      subscriptionClient.sendAck(receivedMessage.getAckID());
+      subscriptionClient.close();
+
+      if(textMessageSent.equals(receivedTextMessage)) {
+        logger.info(String.format("[%s] Test PASSED!!", scenario));
+      }
+      else {
+        logger.info(String.format("[%s] Message did not match. Test FAILED!!", scenario));
+      }
+    }
+    catch (Exception e) {
+      logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
+      e.printStackTrace();
+      logger.error(String.format("[%s] Test FAILED.", scenario));
+    }
+  }
+
+  private static void PS_SUB_MSG_02() {
+    String scenario = "PS_SUB_MSG_02";
+    //URI regionalEndpoint = URI.create("pubsub.us-central1.rep.googleapis.com:443");
+    try {
+      //send message
+      TopicClient tempTopicClient = TopicClient.builder("gcp")
+          .withTopicName(String.format("projects/%s/topics/%s", PROJECT_ID, TOPIC))
+          //.withEndpoint(regionalEndpoint)
+          .build();
+
+      String textMessageSent1 = String.format("Test Message 1 for %s", scenario);
+      String textMessageSent2 = String.format("Test Message 2 for %s", scenario);
+      String textMessageSent3 = String.format("Test Message 3 for %s", scenario);
+      String textMessageSent4 = String.format("Test Message 4 for %s", scenario);
+      String textMessageSent5 = String.format("Test Message 5 for %s", scenario);
+      String textMessageSent6 = String.format("Test Message 6 for %s", scenario);
+
+      Message messageToBeSent1 = Message.builder()
+          .withBody(textMessageSent1.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      Message messageToBeSent2 = Message.builder()
+          .withBody(textMessageSent2.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      Message messageToBeSent3 = Message.builder()
+          .withBody(textMessageSent3.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      Message messageToBeSent4 = Message.builder()
+          .withBody(textMessageSent4.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      Message messageToBeSent5 = Message.builder()
+          .withBody(textMessageSent5.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      Message messageToBeSent6 = Message.builder()
+          .withBody(textMessageSent6.getBytes())
+          .withMetadata("key1", "value1")
+          .build();
+
+      tempTopicClient.send(messageToBeSent1);
+      tempTopicClient.send(messageToBeSent2);
+      tempTopicClient.send(messageToBeSent3);
+      tempTopicClient.send(messageToBeSent4);
+      tempTopicClient.send(messageToBeSent5);
+      tempTopicClient.send(messageToBeSent6);
+      tempTopicClient.close();
+
+      //receive message
+      SubscriptionClient subscriptionClient = SubscriptionClient.builder("gcp")
+          .withSubscriptionName(String.format("projects/%s/subscriptions/%s", PROJECT_ID, SUBSCRIPTION))
+          //.withEndpoint(regionalEndpoint)
+          .build();
+      int count = 0;
+      while (true) {
+        Message message = subscriptionClient.receive();
+        try {
+          String data = new String(message.getBody());
+          // Process the message
+          //processMessage(data);
+          count++;
+
+          // Acknowledge on success
+          subscriptionClient.sendAck(message.getAckID());
+          if(count == 6) {
+            break;
+          }
+        } catch (Exception e) {
+          // Nack on failure for redelivery (if supported)
+          if (subscriptionClient.canNack()) {
+            subscriptionClient.sendNack(message.getAckID());
+          }
+        }
+      }
+      subscriptionClient.close();
     }
     catch (Exception e) {
       logger.error(String.format("[%s] Error during test: %s", scenario, e.getMessage()));
